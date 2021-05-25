f5c906f56e5fffeb0c71b7e52340fab2a357ff42
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-05-25 13:44:59 -0400

    add global styles

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,18 @@ import Head from "next/head";
 import Nav from "../components/ui/Nav";
 import Landing from "../components/landing";
 
+// TODO: temporary component used to test scroll links
+const _placeholderSection = ({ id, idx }) => (
+  <section
+    id={id}
+    style={{
+      height: "60vh",
+      backgroundColor: "rgb(255,0,0)",
+      filter: `hue-rotate(${(idx ?? 0) * 120}deg)`,
+    }}
+  />
+);
+
 export default function Index() {
   return (
     <>
@@ -15,8 +27,13 @@ export default function Index() {
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
+
       <Nav />
       <Landing />
+
+      {["schedule", "about", "speakers", "faqs"].map((id, idx) => (
+        <_placeholderSection id={id} idx={idx} />
+      ))}
     </>
   );
 }
