da653e7c00cdef7730f9298f311e3dd56fbb205f
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-06-05 11:32:14 -0400

    add move landing to separate component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import styles from "../styles/Landing.module.css";
 
-export default function Landing() {
+import Landing from "../components/landing";
+
+export default function Index() {
   return (
-    <div className={styles.landingContainer}>
+    <>
       <Head>
         <title>Reflections Projects 2021</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -13,25 +14,7 @@ export default function Landing() {
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
-
-      <main className={styles.main}>
-        <img src="/logo.png" alt="Reflections Projects Logo" />
-        <h1>
-          reflections <span className={styles.logoBar}>|</span> projections
-        </h1>
-        <p>27 years of connecting students with industry</p>
-        <h2>September 20-25th, 2021</h2>
-      </main>
-
-      <footer className={styles.footer}>
-        Questions? Interested in sponsoring?&nbsp;
-        <a
-          href="mailto:contact@reflectionsprojections.org"
-          rel="noopener noreferrer"
-        >
-          Email us at contact [at] reflectionsprojections.org
-        </a>
-      </footer>
-    </div>
+      <Landing />
+    </>
   );
 }
