42db8cff9eee81df8a0e84598a9a536a33977a8c
Rachael Wei <rachaelwei@gmail.com>
2021-07-13 16:04:00 -0400

    Added offset variables

--- components/Agenda/index.js
+++ components/Agenda/index.js
@@ -33,8 +33,9 @@ if (typeof window !== "undefined") {
   window.addEventListener("mousemove", event => {
     const width = window.outerWidth;
     const height = window.outerHeight;
+    const xOffset = 0;
     const yOffset = 7;
-    const mouseXpercentage = (event.clientX / width * 100);//Math.round(event.pageX / width * 100);
+    const mouseXpercentage = (event.clientX / width * 100) + xOffset;//Math.round(event.pageX / width * 100);
     const mouseYpercentage = (event.clientY / height * 100) + yOffset;//Math.round(event.pageY / height * 100);
     document.getElementsByTagName("body")[0].style = "background: radial-gradient(at " + mouseXpercentage + "% " + mouseYpercentage + "%, #EF7B23, #ffffff)";
   });
