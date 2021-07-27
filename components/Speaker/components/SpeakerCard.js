93b8f1f2eee6cbb127053c7c60097516a075bcae
Daniel <dm32@illinois.edu>
2021-07-27 19:19:29 -0400

    added collapse button

--- components/Speaker/components/SpeakerCard.js
+++ components/Speaker/components/SpeakerCard.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import Badge from 'react-bootstrap/Badge';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { ChevronDown, ChevronUp } from 'react-bootstrap-icons'
 import styles from './SpeakerCard.module.scss';
 import { getQueryObject } from '../../../lib/path';
 
 const SpeakerCard = ({ name, cardImage, tagline, badge, bio }) => {
   const [hrefObj, setHrefObj] = useState(undefined);
+  const [isCollapsed, setIsCollapsed] = useState(true);
   useEffect(() => {
     if (hrefObj === undefined) {
       setHrefObj({
@@ -21,7 +22,6 @@ const SpeakerCard = ({ name, cardImage, tagline, badge, bio }) => {
   });
 
   const badgeJSX = badge ? <Badge variant="primary">{badge}</Badge> : null;
-  console.log(bio);
   return (
 	  
       <div className={styles.speakerCard} key={name}>
@@ -35,13 +35,16 @@ const SpeakerCard = ({ name, cardImage, tagline, badge, bio }) => {
 			  </Col>
 			  <Col sm={10} md={8}>
 				<div className={styles.text}>
-					<div>
+					<div>	
 						<h1 className = {styles.speakerName} >
 						{name}
 						</h1>
 						<h3 className={styles.speakerTagline}>{tagline}</h3>
+						<Button className={styles.collapseButton} onClick={() => setIsCollapsed(!isCollapsed)}>
+							{isCollapsed ?  <ChevronDown/> : <ChevronUp/> }
+						</Button>
 					</div>
-					<div className={styles.bio}>
+					<div variant="outline-primary" className={`${styles.bio} ${isCollapsed ?  styles.collapsed : styles.expanded}`} size="sm">
 						{ReactHtmlParser(bio)}
 					</div>
 				</div>
