0ee3ccfd4685f9f7cc6c3e0e3bba6538c6111432
Rishub Podar <rishubpodar@Rishubs-MacBook-Pro.local>
2021-08-16 13:33:43 -0400

    Added gradient

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,17 @@ import Head from "next/head";
 import Landing from '../components/landing';
 
 export default function Index() {
+  if (typeof window !== "undefined") {
+    window.addEventListener("mousemove", event => {
+      const width = window.outerWidth;
+      const height = window.outerHeight;
+      const xOffset = 0;
+      const yOffset = 7;
+      const mouseXpercentage = (event.clientX / width * 100) + xOffset;//Math.round(event.pageX / width * 100);
+      const mouseYpercentage = (event.clientY / height * 100) + yOffset;//Math.round(event.pageY / height * 100);
+      document.getElementsByTagName("body")[0].style = "background: radial-gradient(at " + mouseXpercentage + "% " + mouseYpercentage + "%, var(--orange-3), var(--yellow-3))";
+    });
+  }
   return (
     <>
         <Head>
