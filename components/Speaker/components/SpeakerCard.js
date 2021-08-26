096f4c0fb76a14c1ba100edc7d6dd00a663cf1f5
Rishub Podar <rishubwork@gmail.com>
2021-08-25 19:00:29 -0500

    Fixed Speakers

--- components/Speaker/components/SpeakerCard.js
+++ components/Speaker/components/SpeakerCard.js
@@ -7,54 +7,10 @@ import styles from './SpeakerCard.module.scss';
 import { getQueryObject } from '../../../lib/path';
 
 const SpeakerCard = ({ name, cardImage, tagline, badge, bio }) => {
-  const useMediaQuery = (query, whenTrue, whenFalse) => {
-    if (
-      typeof window === 'undefined' ||
-      typeof window.matchMedia === 'undefined'
-    )
-      return whenFalse;
-
-    const mediaQuery = window.matchMedia(query);
-    const [match, setMatch] = React.useState(!!mediaQuery.matches);
-
-    React.useEffect(() => {
-      const handler = () => setMatch(!!mediaQuery.matches);
-      mediaQuery.addListener(handler);
-      return () => mediaQuery.removeListener(handler);
-    }, []);
-
-    return match ? whenTrue : whenFalse;
-  };
-
-  const [hrefObj, setHrefObj] = useState(undefined);
-  const [isCollapsed, setIsCollapsed] = useState(true);
-  useEffect(() => {
-    if (hrefObj === undefined) {
-      setHrefObj({
-        pathname: '/speakers',
-        query: {
-          ...getQueryObject(window),
-          name,
-        },
-      });
-    }
-  });
-
-  const badgeJSX = badge ? <Badge variant="primary">{badge}</Badge> : null;
-
-  const speakerBio = useMediaQuery(
-    '(max-width: 599px)',
-    `<details>
-      <summary>Background</summary>
-        ${bio}
-    </details>`,
-    bio
-  );
-
   return (
     <div className={styles.speakerCard} key={name}>
-      <Row style={{ display: 'flex' }}>
-        <Col sm={2} md={4}>
+      <Row style={{ width: '100%' }}>
+        <Col sm={2}>
           <div className={styles.imageContainer}>
             <div className={styles.gradientCircle}>
               <Image
@@ -66,14 +22,17 @@ const SpeakerCard = ({ name, cardImage, tagline, badge, bio }) => {
             </div>
           </div>
         </Col>
-        <Col sm={10} md={8}>
+        <Col sm={10}>
           <div className={styles.text}>
             <div>
-              <h1 className={styles.speakerName}>{name}</h1>
-              <h3 className={styles.speakerTagline}>{tagline}</h3>
+              <h5 className={styles.speakerName}>{name}</h5>
+              <h6 className={styles.speakerTagline}>{tagline}</h6>
             </div>
             <div variant="outline-primary" className={styles.bio} size="sm">
-              {ReactHtmlParser(speakerBio)}
+              <details>
+                <summary>Background</summary>
+                {ReactHtmlParser(bio)}
+              </details>
             </div>
           </div>
         </Col>
--- components/Speaker/index.js
+++ components/Speaker/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Section from '../../UIComponents/Section';
+import Section from '../Section';
 
 import SpeakerCard from './components/SpeakerCard';
 
@@ -17,35 +15,28 @@ const Speaker = ({ speakers }) => {
       </Section.Header>
       <Section.Body>
         <Container>
-          <Row>
-            {speakers.length === 0 ? (
-              <h4>Loading...</h4>
-            ) : (
-              <>
-                {speakers.map((speaker) => {
-                  const { name, tagline, badge, bio, image } = speaker;
-                  const imageURL = `${image}`;
-                  return (
-                    <Col
-                      className={styles.speakerCardContainer}
-                      sm={12}
-                      md={6}
-                      lg={4}
-                    >
-                      <SpeakerCard
-                        name={name}
-                        cardImage={imageURL}
-                        tagline={tagline}
-                        badge={badge}
-                        key={speaker.name}
-                        bio={bio}
-                      />
-                    </Col>
-                  );
-                })}
-              </>
-            )}
-          </Row>
+          {speakers.length === 0 ? (
+            <h4>Loading...</h4>
+          ) : (
+            <>
+              {speakers.map((speaker) => {
+                const { name, tagline, badge, bio, image } = speaker;
+                const imageURL = `${image}`;
+                return (
+                  <div className={styles.speakerCardContainer} sm={12} md={6}>
+                    <SpeakerCard
+                      name={name}
+                      cardImage={imageURL}
+                      tagline={tagline}
+                      badge={badge}
+                      key={speaker.name}
+                      bio={bio}
+                    />
+                  </div>
+                );
+              })}
+            </>
+          )}
         </Container>
       </Section.Body>
     </Section>
