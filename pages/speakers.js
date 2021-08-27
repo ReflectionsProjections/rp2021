9fe890754fc8076e51075c2b40d0966a23f750e5
Rishub Podar <rishubwork@gmail.com>
2021-08-26 20:53:39 -0500

    Bug fixing

--- pages/speakers.js
+++ pages/speakers.js
@@ -13,13 +13,11 @@ export default function Speakers() {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
           rel="stylesheet"
