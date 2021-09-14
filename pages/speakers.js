c9c28850abde2592c0312591fcdf6190b4032022
Rishub Podar <rishubwork@gmail.com>
2021-09-13 21:46:54 -0500

    fix compile error

--- pages/speakers.js
+++ pages/speakers.js
@@ -13,7 +13,11 @@ export default function Speakers() {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&display=swap"
           rel="stylesheet"
