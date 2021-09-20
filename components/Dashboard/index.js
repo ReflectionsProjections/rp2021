595fee26471bae362471db0a60642fa076f2cea3
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-09-20 12:16:56 -0500

    add update dashboard

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -1,171 +1,127 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Container, Image, Row } from 'react-bootstrap';
 import { Transition } from 'react-transition-group';
 import { Timeline } from 'react-twitter-widgets';
 
+import useGetStaticData from '../../services/useGetStaticData';
+import Agenda from '../Agenda';
 import classNames from 'classnames';
-import styles from './Dashboard.module.scss';
-import DashboardClock from './components/dashboardClock';
-import DashboardEvents from './components/dashboardEvents';
+// import styles from './Dashboard.module.scss';
+import styles from './newDashboard.module.scss';
 
-export default class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      logos: (props.sponsorImages ?? []).slice(0, 8),
-      visibles: [true, true, true, true, true, true, true, true],
-      enter: 750,
-      exit: 750,
-    };
+function getDashboardEvents(events) {
+  if (!events) {
+    [[], {}, []];
   }
 
-  componentWillMount() {
-    setInterval(() => {
-      const { visibles } = this.state;
-      const index = Math.floor(Math.random() * visibles.length);
-      visibles[index] = false;
+  const eventObjects = events.allIds.map((id) => events.byId[id]);
+  eventObjects.sort((eventA, eventB) => {
+    return +new Date(eventA.time.start) - +new Date(eventB.time.start);
+  });
 
-      this.setState({ visibles });
-    }, 5000);
-  }
+  let currentOrNext = eventObjects[0];
 
-  randomLogos = (index) => {
-    const { sponsorImages } = this.props;
+  const now = +new Date('09-21-2021 16:30');
 
-    if (!sponsorImages) return;
-
-    const { logos, visibles } = this.state;
-    const allLogos = sponsorImages;
-
-    let newLogoIndex = Math.floor(Math.random() * allLogos.length);
-    let newLogo = allLogos[newLogoIndex];
-    while (logos.indexOf(newLogo) >= 0) {
-      newLogoIndex = Math.floor(Math.random() * allLogos.length);
-      newLogo = allLogos[newLogoIndex];
+  for (let i = 1; i < eventObjects.length; i++) {
+    const event = eventObjects[i];
+    if (+new Date(event.time.end) > now) {
+      currentOrNext = eventObjects[i];
+      break;
     }
-
-    logos[index] = allLogos[newLogoIndex];
-    visibles[index] = true;
-    this.setState({
-      logos,
-      visibles,
-    });
-  };
-
-  render() {
-    const { logos, visibles, enter, exit } = this.state;
-    const { events } = this.props;
-
-    return (
-      <Container className={classNames(styles.dashboardContainer, styles.x)}>
-        <Row className={styles.timeRow}>
-          <Col className={styles.logoColumn}>
-            <Image className={styles.rpLogo} src="/logo.png" />
-          </Col>
-          <Col lg={4}>
-            <DashboardClock clock title="Current Time" />
-          </Col>
-          <Col className={styles.logoColumn}>
-            <Image className={styles.rpWordmark} src="/wordmarkblack.svg" />
-          </Col>
-        </Row>
-        <Row className={styles.mainRow}>
-          <Col>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(0)}
-                in={visibles[0]}
-              >
-                <Image className={styles.logo} src={logos[0]} fluid />
-              </Transition>
-            </Row>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(1)}
-                in={visibles[1]}
-              >
-                <Image className={styles.logo} src={logos[1]} fluid />
-              </Transition>
-            </Row>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(2)}
-                in={visibles[2]}
-              >
-                <Image className={styles.logo} src={logos[2]} fluid />
-              </Transition>
-            </Row>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(3)}
-                in={visibles[3]}
-              >
-                <Image className={styles.logo} src={logos[3]} fluid />
-              </Transition>
-            </Row>
-          </Col>
-          <Col className={styles.eventsColumn} lg={3}>
-            <DashboardEvents events={events} />
-          </Col>
-          <Col className={styles.twitterContainer} lg={3}>
-            <h2 className={styles.twitterTitle}>Twitter</h2>
-            <div className={styles.twitterInnerContainer}>
-              <Timeline
-                dataSource={{ sourceType: 'profile', screenName: 'uiuc_rp' }}
-                options={{
-                  chrome: 'noborders nofooter noheader noscrollbar',
-                  tweetLimit: 4,
-                  ariaPolite: 'rude',
-                  username: 'uiuc_rp',
-                }}
-              />
-            </div>
-          </Col>
-          <Col>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(4)}
-                in={visibles[4]}
-              >
-                <Image className={styles.logo} src={logos[4]} fluid />
-              </Transition>
-            </Row>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(5)}
-                in={visibles[5]}
-              >
-                <Image className={styles.logo} src={logos[5]} fluid />
-              </Transition>
-            </Row>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(6)}
-                in={visibles[6]}
-              >
-                <Image className={styles.logo} src={logos[6]} fluid />
-              </Transition>
-            </Row>
-            <Row className={`justify-content-center ${styles.logoRow}`}>
-              <Transition
-                timeout={{ enter, exit }}
-                onExit={() => this.randomLogos(7)}
-                in={visibles[7]}
-              >
-                <Image className={styles.logo} src={logos[7]} fluid />
-              </Transition>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    );
   }
+
+  const before = eventObjects
+    .filter((event) => {
+      return +new Date(event.time.start) < +new Date(currentOrNext.time.start);
+    })
+    .slice(-3);
+
+  const after = eventObjects
+    .filter((event) => {
+      return +new Date(event.time.start) > +new Date(currentOrNext.time.start);
+    })
+    .slice(0, 3);
+
+  return [before, currentOrNext, after];
+}
+
+/*
+ * adapted from:
+ * https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
+ */
+function formatAMPM(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  return hours + ':' + minutes + ' ' + ampm;
+}
+
+function Event(event) {
+  const {
+    title,
+    description,
+    time: { start, end },
+  } = event;
+
+  return (
+    <div className={styles.event}>
+      <div className={styles.eventTitle}>{title}</div>
+      <div className={styles.eventDescription}>{description}</div>
+      <div className={styles.eventTime}>
+        {formatAMPM(new Date(start))} - {formatAMPM(new Date(end))}
+      </div>
+    </div>
+  );
+}
+
+function DashboardEvents({ events }) {
+  const [eventInfo, setEventInfo] = useState(getDashboardEvents(events));
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setEventInfo(getDashboardEvents(events));
+    }, 3600);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const [before, curr, after] = eventInfo;
+  return (
+    <>
+      <div className={styles.before}>
+        {before.map((event) => (
+          <Event key={event.id} {...event} />
+        ))}
+      </div>
+      <div className={styles.current}>
+        {curr && <Event key={curr.id} {...curr} />}
+      </div>
+      <div className={styles.after}>
+        {after.map((event) => (
+          <Event key={event.id} {...event} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default function Dashboard() {
+  const { isLoaded, rpData } = useGetStaticData();
+
+  const { events } = rpData;
+
+  if (!isLoaded) return null;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>test</div>
+      <div className={styles.right}>
+        <DashboardEvents events={events} />
+      </div>
+      <div className={styles.bottom}>sponsors</div>
+    </div>
+  );
 }
